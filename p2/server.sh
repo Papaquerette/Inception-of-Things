00f875164e8c2d7d642630395b6acea3de732869
cdutel-l <cdutel-l@iot.myguest.virtualbox.org>
2025-04-29 15:10:55 +0200

    Finish part 2 😁

--- p2/server.sh
+++ p2/server.sh
@@ -10,4 +10,8 @@ curl -sfL https://get.k3s.io | sh -
 
 kubectl apply -f /vagrant/confs/app1/deployment.yaml
 kubectl apply -f /vagrant/confs/app1/service.yaml
+kubectl apply -f /vagrant/confs/app2/deployment.yaml
+kubectl apply -f /vagrant/confs/app2/service.yaml
+kubectl apply -f /vagrant/confs/app3/deployment.yaml
+kubectl apply -f /vagrant/confs/app3/service.yaml
 kubectl apply -f /vagrant/confs/ingress.yaml
