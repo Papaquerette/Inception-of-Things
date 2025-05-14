ad5cd89fc2dd16bc062c106301a58970049c2269
Benoit Roux <42benoit.rouxgmail.com>
2025-05-14 13:08:33 +0200

    fix p1 scripts

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -3,4 +3,4 @@
 apt-get update
 apt-get install -y curl
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-external-ip $WORKER_IP" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-external-ip $SERVER_IP" sh -s -
