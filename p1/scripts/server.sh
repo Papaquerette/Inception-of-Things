c0662bdd5bb5bd75222a475151f0014b490ac1c4
beroux <42benoit.roux@gmail.com>
2025-06-22 15:15:13 +0200

    Fix p1 server script

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -4,3 +4,5 @@ apt-get update
 apt-get install -y curl
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-external-ip $SERVER_IP" sh -s -
+
+cp /var/lib/rancher/k3s/server/token /vagrant/token
