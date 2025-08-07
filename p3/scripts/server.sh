0e4f897fb9174bc00188ec381abea59979465f0b
Benoit Roux <42benoit.rouxgmail.com>
2025-08-07 15:16:23 +0200

    refactor p3 server script

--- p3/scripts/server.sh
+++ p3/scripts/server.sh
@@ -6,9 +6,9 @@ pushd $CONFS_ROOT
 
 k3d cluster create -c ./cluster.yaml
 kubectl create -f ./namespaces.yaml
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f ./argocd-config-map.yaml
-kubectl rollout restart deploy argocd-server -n argocd
+kubectl -n argocd rollout restart deploy argocd-server
 kubectl -n argocd rollout restart deploy argocd-repo-server
 kubectl apply -f ./application.yaml
 kubectl apply -f ./ingress.yaml
